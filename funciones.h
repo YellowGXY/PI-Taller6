e48487f80fe2e6319192103f6f57270eabd5b30a
Mateo Pachacama <mateogab7@gmail.com>
2025-05-09 22:56:53 -0500

    Update funciones.h

--- funciones.h
+++ funciones.h
@@ -1,10 +1,13 @@
-int ingresarDatos(char nombres[][30], float precios[]);
-int agregarProductos(char nombres[][30], float precios[], int cantidadActual, int n);
-int eliminarProducto(char nombres[][30], float precios[], int cantidad);
-float calcularTotal(float precios[], int cantidad);
-float calcularPromedio(float precios[], int cantidad);
-int encontrarMasCaro(float precios[], int cantidad);
-int encontrarMasBarato(float precios[], int cantidad);
-void buscarProducto(char nombres[][30], float precios[], int cantidad);
-void mostrarInventario(char nombres[][30], float precios[], int cantidad);
 void mostrarMenu();
+
+int ingresarDatos(char nombres[][30], float precios[], int stocks[]);
+int eliminarProducto(char nombres[][30], float precios[], int stocks[], int cantidad);
+
+float calcularTotal(float precios[], int stocks[], int cantidad);
+float calcularPromedio(float precios[], int stocks[], int cantidad);
+
+int encontrarMasCaro(float precios[], int stocks[], int cantidad);
+int encontrarMasBarato(float precios[], int stocks[], int cantidad);
+
+int buscarProducto(char nombres[][30], float precios[], int stocks[], int cantidad);
+void agregarStock(char nombres[][30], float precios[], int stocks[], int cantidad);
